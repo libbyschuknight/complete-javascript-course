cfceb94cf935c21c8d93720a077d217ecdbaa849
Libby Schumacher-Knight <libbysk@gmail.com>
2026-06-15 17:34:20 +1200

    Section 10: closer look at functions 136

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+// const bookings = [];
+
+// const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// }
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// // createBooking('LH123', 2);
+// // createBooking('LH123', 5);
+// createBooking('LH123', undefined, 1000); // skip default value for numPassengers
+
+
+const flight = 'LH234'; // value / primitive type
+const jonas = { // object / reference type
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+}
+newPassport(jonas);
+checkIn(flight, jonas);
+
+
+// passing by value and passing by reference
+// primitive types are passed by value, meaning that a copy of the value is created and passed to the function.
+// objects are passed by reference, meaning that a reference to the object is passed to the function, allowing the function to modify the original object.
+// JS does not have pass by reference, but it has pass by value for objects, which means that the reference to the object is passed by value.
